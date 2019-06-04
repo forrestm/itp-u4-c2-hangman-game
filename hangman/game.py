46878e10f865255d31b74cfea12f98d4644b7c2e
Forrest <11200528+forrestm@users.noreply.github.com>
2019-06-04 05:52:17 -0500

    Updating game.py

--- hangman/game.py
+++ hangman/game.py
@@ -1,19 +1,28 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ['chicken', 'horse', 'donkey', 'monkey', 'lion']
 
 
 def _get_random_word(list_of_words):
-    pass
+    return random.choice(list_of_words)
 
 
 def _mask_word(word):
-    pass
+    secret_word = ''
+    for letter in word:
+        secret_word += '*'
+    return secret_word
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if len(answer_word) == 0:
+        raise InvalidWordException
+    elif len(character) > 1:
+        raise InvalidGuessedLetterException
+    elif len(answer_word) != len(masked_word):
+        raise InvalidWordException
 
 
 def guess_letter(game, letter):
@@ -34,3 +43,4 @@ def start_new_game(list_of_words=None, number_of_guesses=5):
     }
 
     return game
+
